b197d2276c24080affaf6252478c3f102e388a90
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-04-06 14:00:40 +0200

    Refactored code

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,12 +1,13 @@
 public class Example {
     private final int number1;
     private final int number2;
-    Example(int num1, int num2){
+
+    Example(int num1, int num2) {
         number1 = num1;
         number2 = num2;
     }
 
-    int getSum(){
+    int getSum() {
         return number1 + number2;
     }
 }
