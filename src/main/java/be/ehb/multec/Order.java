e87638d915c2ff7f6f65dc4d018f00f3216d2f14
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-04-14 12:15:59 +0200

    changed imports, SRP added

--- src/main/java/be/ehb/multec/Order.java
+++ src/main/java/be/ehb/multec/Order.java
@@ -2,22 +2,36 @@ package be.ehb.multec;
 
 import java.util.ArrayList;
 
-public class Order{
+import static java.lang.System.err;
+
+public class Order {
     private double cost;
-    private String description = "Uw order bevat: ";
+    private String description = "Your order contains: ";
 
     Order(ArrayList<Fries> orderList) {
+        int orderListLength = orderList.size();
         for (Fries order : orderList) {
-            this.cost += Math.round(order.cost());
-            this.description += order.description;
+            calculateCostAndDescription(order);
+            if (orderList.lastIndexOf(order) != orderListLength - 1)
+                this.description += ", ";
         }
     }
 
-    public double getCost(){
+    private void calculateCostAndDescription(Fries order){
+        if (order != null) {
+            this.cost += Math.round(order.cost());
+            this.description += order.getDescription();
+        } else {
+            System.out.println(" ** Your order should not contain a nullified object.");
+            this.description += "null";
+        }
+    }
+
+    public double getCost() {
         return cost;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
 }
