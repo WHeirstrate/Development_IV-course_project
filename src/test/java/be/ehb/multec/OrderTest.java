f952b96e6c22d11ff4e99f76048e460ec6e8e4c7
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-04-14 12:16:15 +0200

    changed imports, SRP added

--- src/test/java/be/ehb/multec/OrderTest.java
+++ src/test/java/be/ehb/multec/OrderTest.java
@@ -1,25 +1,38 @@
 package be.ehb.multec;
 
+import be.ehb.multec.sauce.Ketchup;
+import be.ehb.multec.sauce.Mayo;
+import be.ehb.multec.sauce.Special;
+import be.ehb.multec.size.Family;
+import be.ehb.multec.size.Medium;
+import be.ehb.multec.size.Small;
+import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class OrderTest {
+    ArrayList<Fries> orderList = new ArrayList<>();
+    Fries order1 = new Special(new Family());
+    Fries order2 = new Mayo(new Medium());
+    Fries order3 = new Ketchup(new Small());
 
     @Test
     public void getCost() {
-        ArrayList<Fries> orderList = new ArrayList<>();
-
-        Fries order1 = new Mayo(new Medium());
-        Fries order2 = new Special(new Medium());
-
-        orderList.add(order1);
+        orderList.add(null);
         orderList.add(order2);
 
         Order order = new Order(orderList);
-        System.out.println(order.getCost());
+        assertEquals(4.0, order.getCost());
+    }
+
+    @Test
+    public void getDescription(){
+        orderList.add(null);
+        orderList.add(order3);
+        Order order = new Order(orderList);
         System.out.println(order.getDescription());
     }
 }
