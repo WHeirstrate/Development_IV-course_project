2cbc17919ab582868618a53a578867cbad8f7264
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-04-17 14:26:40 +0200

    Moved enum to own file

---
+++ Javascript/enums.js
@@ -0,0 +1,46 @@
+const sizeEnum = {
+    small: {
+        name: "small",
+        cost: 2.10,
+        time: 3,
+        description: "Small fries"
+    },
+    medium: {
+        name: "medium",
+        cost: 3.10,
+        time: 3,
+        description: "Medium fries"
+    },
+    family: {
+        name: "family",
+        cost: 5.10,
+        time: 3,
+        description: "Family fries"
+    }
+};
+
+const sauceEnum = {
+    mayo: {
+        name: "mayonnaise",
+        cost: 0.70,
+        time: 1,
+        description: " with mayonaise"
+    },
+    ketchup: {
+        name: "ketchup",
+        cost: 0.70,
+        time: 1,
+        description: " with ketchup"
+    },
+    special: {
+        name: "special sauce",
+        cost: 1.50,
+        time: 2,
+        description: " with special sauce"
+    }
+};
+
+export {
+    sizeEnum,
+    sauceEnum
+};
