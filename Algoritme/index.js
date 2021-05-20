fdf0f9a20c3c30a99faba5ac8a2e8d1b8f41f50d
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-05-20 15:46:45 +0200

    added missing number algoritme

--- Algoritme/index.js
+++ Algoritme/index.js
@@ -1,1 +1,13 @@
-console.log('Algoritme');
+let array = [12, 13, 14, 19, 22, 15];
+//-----------------------------------------//
+function findMissing(array) {
+    array.sort((a, b) => a - b);
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] !== array[i + 1] - 1) {
+            return array[i] + 1;
+        }
+    }
+    return array;
+}
+//-----------------------------------------//
+console.log(findMissing(array));
