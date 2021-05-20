f5abd61f098adee75ba1a84e0ea32002a2481e8e
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-05-20 16:48:26 +0200

    Link form to array

--- Algoritme/index.js
+++ Algoritme/index.js
@@ -1,13 +1,31 @@
-let array = [12, 13, 14, 19, 22, 15];
+let array = [];
+
+document.getElementById('inputForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    let number = document.getElementById('numberInput');
+    array.push(number.value)
+    document.getElementById('inputForm').reset();
+    updateNumbers();
+});
+
+function updateNumbers() {
+    let htmlEl = document.getElementById('displayNumbers');
+    htmlEl.innerText = array;
+}
+
 //-----------------------------------------//
+/* Find first missing number in the array,
+ * starting from the smallest number.
+ */
 function findMissing(array) {
     array.sort((a, b) => a - b);
+    console.log(array);
     for (let i = 0; i <= array.length; i++) {
-        if (array[i] !== array[i + 1] - 1) {
+        if (array[i] !== array[i + 1] - 1 && array[i] !== array[i + 1]) {
             return array[i] + 1;
         }
     }
     return array;
 }
 //-----------------------------------------//
-console.log(findMissing(array));
+//console.log(findMissing(array));
