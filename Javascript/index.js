d6c9cc5d3f222bb5f2ebd3f0ddf11a4046ab0dda
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-05-21 10:26:16 +0200

    Refactored code

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,61 +1,61 @@
 const sizes = {
   small: {
-      name: "small",
-      cost: 2.10,
-      time: 3,
-      description: "Small fries"
+    name: "small",
+    cost: 2.10,
+    time: 3,
+    description: "Small fries"
   },
   medium: {
-      name: "medium",
-      cost: 3.10,
-      time: 3,
-      description: "Medium fries"
+    name: "medium",
+    cost: 3.10,
+    time: 3,
+    description: "Medium fries"
   },
   family: {
-      name: "family",
-      cost: 5.10,
-      time: 3,
+    name: "family",
+    cost: 5.10,
+    time: 3,
   }
 };
 
 const sauces = {
   mayo: {
-      name: "mayonnaise",
-      cost: 0.70,
-      time: 1,
+    name: "mayonnaise",
+    cost: 0.70,
+    time: 1,
   },
   ketchup: {
-      name: "ketchup",
-      cost: 0.70,
-      time: 1,
+    name: "ketchup",
+    cost: 0.70,
+    time: 1,
   },
   special: {
-      name: "special",
-      cost: 1.50,
-      time: 2,
+    name: "special",
+    cost: 1.50,
+    time: 2,
   }
 };
 
 
-function getOrderNames(fries){
-  return function (sauce){
-  let newSauces = sauce.map(x => x.name);
-   return [fries.name, ...newSauces]
+function getOrderNames(fries) {
+  return function (sauce) {
+    let newSauces = sauce.map(x => x.name);
+    return [fries.name, ...newSauces]
   }
 }
 
 
-function getDescription(p){
-  return p.map((x,i) => {
-    if(i == 0)
+function getDescription(p) {
+  return p.map((x, i) => {
+    if (i == 0)
       return x + " fries with";
-   else  if(i+1 != p.length)
-    return x + " and";
+    else if (i + 1 != p.length)
+      return x + " and";
     return x
   })
 }
 
-function allSauces(...sauces){
+function allSauces(...sauces) {
   return sauces
 }
 
