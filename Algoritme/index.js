938a51f4fde4778a1eaaaf3b3fae9e9fb05d4789
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-05-23 16:40:07 +0200

    Add scramble with DOM and comments

--- Algoritme/index.js
+++ Algoritme/index.js
@@ -1,5 +1,14 @@
+//-----------------------------------------//
+//------------ GLOBAL VARIABLES -----------//
+//-----------------------------------------//
+
 let array = [];
 
+//-----------------------------------------//
+//------------------- DOM -----------------//
+//-----------------------------------------//
+
+// Take input from form, add to array and call function to display new array
 document.getElementById('inputForm').addEventListener('submit', (event) => {
     event.preventDefault();
     let number = document.getElementById('numberInput');
@@ -8,22 +17,36 @@ document.getElementById('inputForm').addEventListener('submit', (event) => {
     updateNumbers();
 });
 
+// Display the global array on screen
 function updateNumbers() {
     let htmlEl = document.getElementById('displayNumbers');
     htmlEl.innerText = array.join(', ');
 }
-
+// On button press, scramble the global array and display it
+document.getElementById('scramble').addEventListener('click', (event) => {
+    event.preventDefault();
+    const scrambledArray = scrambleArray(array);
+    document.getElementById('scrambledArray').innerText = scrambledArray.join(', ');
+});
+// On button press, call the algorithm to find - to then display - the missing number
 document.getElementById('sortAndFind').addEventListener('click', () => {
     const missingNr = findMissing(array);
-    console.log(array);
     document.getElementById('missingNr').innerText = missingNr;
 })
 
 //-----------------------------------------//
-//---------------- ALGORITME --------------//
+//---------------- ALGORITHM --------------//
 //-----------------------------------------//
 
-/* Find first missing number in the array,
+/* 
+ * Randomize the array
+ */
+function scrambleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+/*
+ * Find first missing number in the array,
  * starting from the smallest number.
  */
 function findMissing(array) {
